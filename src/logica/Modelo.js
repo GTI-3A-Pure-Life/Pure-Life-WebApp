7ce2650462d8e7f8d5499f4741ec1570198c5ad1
Ruben <rubenpardocasanova@gmail.com>
2021-11-19 10:26:58 +0100

    Arreglar formato fecha 19/11/2021

--- src/logica/Modelo.js
+++ src/logica/Modelo.js
@@ -333,8 +333,8 @@ class Posicion{
     let date = new Date(fechaAFormatear);
     let strRes = 
     (date.getFullYear()+
-    "/"+(date.getMonth()+1)+
-    "/"+date.getDate()+
+    "-"+(date.getMonth()+1)+
+    "-"+date.getDate()+
     " "+((date.getHours() < 10 ? "0" : "") + date.getHours())+
     ":"+((date.getMinutes() < 10 ? "0" : "") + date.getMinutes())+
     ":"+((date.getSeconds() < 10 ? "0" : "") + date.getSeconds()));
